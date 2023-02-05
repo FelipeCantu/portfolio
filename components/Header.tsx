ddb4d73376b34f4e599f432b616a8d925ed5da9f
FelipeCantu <felipecantujr@Felipes-MBP.lan>
2023-02-05 13:54:51 -0600

    hero

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
-export default function Header({ }: Props) {
+export default function Header({}: Props) {
     return (
-        <header className='sticky top-0 flex items-center justify-between max-w-7xl mx-auto z-20'>
-            <div className='flex flex-row items-center'>
+        <header className='sticky top-0 p-5 flex items-center justify-between max-w-7xl mx-auto z-20
+        xl:items-center'>
+            <motion.div 
+                initial={{
+                    x: -500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1
+                }}
+                transition={{
+                    duration: 1.5,
+                }}
+                className='flex flex-row items-center'
+            >
                 {/* Social Icons */}
                 <SocialIcon
                     url='https://www.instagram.com/son_of_robot_/'
@@ -33,8 +50,23 @@ export default function Header({ }: Props) {
                     fgColor='Gray'
                     bgColor='Transparent'
                 />
-            </div>
-            <div className='flex flex-row items-center text-gray-300 cursor-ponter'>
+            </motion.div>
+            <motion.div 
+                initial={{
+                    x: 500,
+                    opacity: 0,
+                    scale: 0.5
+                }}
+                animate={{
+                    x: 0,
+                    opacity: 1,
+                    scale: 1,
+                }}
+                transition={{
+                    duration: 1
+                }}
+                className='flex flex-row items-center text-gray-300 cursor-ponter'
+            >
                 <SocialIcon
                     className='cursor-pointer'
                     network='email'
@@ -42,7 +74,7 @@ export default function Header({ }: Props) {
                     bgColor='Transparent'
                 />
                 <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Keep In Touch</p>
-            </div>
+            </motion.div>
         </header>
 
     )
